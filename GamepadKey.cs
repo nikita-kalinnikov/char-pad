9fabb5f0bb3f0d4b0e3d296dd8a3b59e2212ef4a
Nikita Kalinnikov <niki9888985@mail.ru>
2022-11-07 02:10:02 +0300

    add GamepadKey enum

---
+++ GamepadKey.cs
@@ -0,0 +1,22 @@
+﻿namespace CharPad
+{
+    internal enum GamepadKey
+    {
+        None = 0,
+
+        DPad,
+
+        LeftTrigger,
+        LeftBumper,
+        LeftStick,
+
+        RightTrigger,
+        RightBumper,
+        RightStick,
+
+        X, Y, B, A,
+
+        Start,
+        Back
+    }
+}
