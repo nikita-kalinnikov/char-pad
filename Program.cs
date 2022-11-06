1df1847c5db18e543513702407e9e389c3ee2278
Nikita Kalinnikov <niki9888985@mail.ru>
2022-11-07 01:40:31 +0300

    remove released control points

--- Program.cs
+++ Program.cs
@@ -21,52 +21,34 @@ var updates = Array.Empty<JoystickUpdate>();
 var bindings = new Bindings();
 
 
-bindings.Start_Pressed.Subscribe(() => Console.WriteLine($"Start button pressed"));
-bindings.Start_Released.Subscribe(() => Console.WriteLine($"Start button released"));
+bindings.Start.Subscribe(() => Console.WriteLine($"Start button pressed"));
+bindings.Back.Subscribe(() => Console.WriteLine($"Back button pressed"));
 
-bindings.Back_Pressed.Subscribe(() => Console.WriteLine($"Back button pressed"));
-bindings.Back_Released.Subscribe(() => Console.WriteLine($"Back button released"));
-
-
-bindings.Left_Trigger_Pressed.Subscribe(() => Console.WriteLine($"Left Trigger pressed"));
-bindings.Left_Stick_Pressed.Subscribe(() => Console.WriteLine($"Left Stick pressed"));
-bindings.Left_Stick_Released.Subscribe(() => Console.WriteLine($"Left Stick released"));
+bindings.Left_Trigger.Subscribe(() => Console.WriteLine($"Left Trigger pressed"));
+bindings.Left_Stick.Subscribe(() => Console.WriteLine($"Left Stick pressed"));
 bindings.Left_Stick_Up.Subscribe(() => Console.WriteLine($"Left Stick up"));
 bindings.Left_Stick_Down.Subscribe(() => Console.WriteLine($"Left Stick down"));
 bindings.Left_Stick_Left.Subscribe(() => Console.WriteLine($"Left Stick left"));
 bindings.Left_Stick_Right.Subscribe(() => Console.WriteLine($"Left Stick right"));
-bindings.Left_Bumper_Pressed.Subscribe(() => Console.WriteLine($"Left Bumper pressed"));
-bindings.Left_Bumper_Released.Subscribe(() => Console.WriteLine($"Left Bumper released"));
+bindings.Left_Bumper.Subscribe(() => Console.WriteLine($"Left Bumper pressed"));
 
-
-bindings.Right_Trigger_Pressed.Subscribe(() => Console.WriteLine($"Right Trigger pressed"));
-bindings.Right_Stick_Pressed.Subscribe(() => Console.WriteLine($"Right Stick pressed"));
-bindings.Right_Stick_Released.Subscribe(() => Console.WriteLine($"Right Stick released"));
+bindings.Right_Trigger.Subscribe(() => Console.WriteLine($"Right Trigger pressed"));
+bindings.Right_Stick.Subscribe(() => Console.WriteLine($"Right Stick pressed"));
 bindings.Right_Stick_Up.Subscribe(() => Console.WriteLine($"Right Stick up"));
 bindings.Right_Stick_Down.Subscribe(() => Console.WriteLine($"Right Stick down"));
 bindings.Right_Stick_Left.Subscribe(() => Console.WriteLine($"Right Stick left"));
 bindings.Right_Stick_Right.Subscribe(() => Console.WriteLine($"Right Stick right"));
-bindings.Right_Bumper_Pressed.Subscribe(() => Console.WriteLine($"Right Bumper pressed"));
-bindings.Right_Bumper_Released.Subscribe(() => Console.WriteLine($"Right Bumper released"));
+bindings.Right_Bumper.Subscribe(() => Console.WriteLine($"Right Bumper pressed"));
 
+bindings.X.Subscribe(() => Console.WriteLine($"X button pressed"));
+bindings.Y.Subscribe(() => Console.WriteLine($"Y button pressed"));
+bindings.B.Subscribe(() => Console.WriteLine($"B button pressed"));
+bindings.A.Subscribe(() => Console.WriteLine($"A button pressed"));
 
-bindings.X_Pressed.Subscribe(() => Console.WriteLine($"X button pressed"));
-bindings.X_Released.Subscribe(() => Console.WriteLine($"X button released"));
-
-bindings.Y_Pressed.Subscribe(() => Console.WriteLine($"Y button pressed"));
-bindings.Y_Released.Subscribe(() => Console.WriteLine($"Y button released"));
-
-bindings.B_Pressed.Subscribe(() => Console.WriteLine($"B button pressed"));
-bindings.B_Released.Subscribe(() => Console.WriteLine($"B button released"));
-
-bindings.A_Pressed.Subscribe(() => Console.WriteLine($"A button pressed"));
-bindings.A_Released.Subscribe(() => Console.WriteLine($"A button released"));
-
-
-bindings.DPad_Up_Pressed.Subscribe(() => Console.WriteLine($"DPad Up pressed"));
-bindings.DPad_Down_Pressed.Subscribe(() => Console.WriteLine($"DPad Down pressed"));
-bindings.DPad_Left_Pressed.Subscribe(() => Console.WriteLine($"DPad Left pressed"));
-bindings.DPad_Right_Pressed.Subscribe(() => Console.WriteLine($"DPad Right pressed"));
+bindings.DPad_Up.Subscribe(() => Console.WriteLine($"DPad Up pressed"));
+bindings.DPad_Down.Subscribe(() => Console.WriteLine($"DPad Down pressed"));
+bindings.DPad_Left.Subscribe(() => Console.WriteLine($"DPad Left pressed"));
+bindings.DPad_Right.Subscribe(() => Console.WriteLine($"DPad Right pressed"));
 
 
 while (true)
