8c2e2b5d62be3978305f91372bea65444a2039c8
Nikita Kalinnikov <niki9888985@mail.ru>
2022-11-06 23:59:53 +0300

    add empty input map json

---
+++ InputMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CharPad
+{
+    internal struct InputMap
+    {
+        private const string Path = "input_map.json";
+
+
+        [JsonConstructor]
+        public InputMap() { }
+
+
+        public static InputMap Read()
+        {
+            if (File.Exists(Path) is false) throw new FileNotFoundException($"Input map file was not found at path: {Path}");
+
+            var inputMapJson = File.ReadAllText(Path, Encoding.UTF8).Trim();
+            var inputMap = JsonSerializer.Deserialize<InputMap>(inputMapJson);
+
+            return inputMap;
+        }
+    }
+}
